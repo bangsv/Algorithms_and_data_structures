bb802812d343625975b981affebda3bba8212c39
Захарчук Юрий <zahar.yury11971@gmail.com>
2023-01-27 13:11:07 +0300

    new_func

--- Computational_Mathematics/LB_1/class_Matrix/Matrix.h
+++ Computational_Mathematics/LB_1/class_Matrix/Matrix.h
@@ -2,49 +2,52 @@
 #include <algorithm> 
 #include <iostream>
 #include <initializer_list>
-#include <cmath>
+#include <cmath> 
 
+/**
+ * Matrix class
+ * 
+ * This class is used to represent a matrix of numbers. It provides methods for performing 
+ * operations on the matrix such as addition, subtraction, multiplication, and division.
+ */
 class Matrix {
 public:
+/**
+ * Matrix() is a constructor that creates a new Matrix object.
+ */
     Matrix() {}
 
-    Matrix(int rows, int columns, int r) {
-        for (int i = 0; i < rows; i++) {
-            std::vector<double> row;
-            for (int j = 0; j < columns; j++) {
-                row.push_back(rand() % r * (double)1);
-            }
-            matrix.push_back(row);
-        }
-    }
+/**
+ * Constructor for Matrix class
+ * @param rows - number of rows in the matrix
+ * @param columns - number of columns in the matrix
+ * @param r - maximum random value for each element in the matrix
+ */
+    Matrix(int rows, int columns, int r);
 
-    Matrix(int rows, int columns) {
-        for (int i = 0; i < rows; i++) {
-            std::vector<double> row;
-            for (int j = 0; j < columns; j++) {
-                row.push_back(0);
-            }
-            matrix.push_back(row);
-        }
-    }
+/**
+ * Constructor for Matrix object.
+ * 
+ * @param rows The number of rows in the matrix.
+ * @param columns The number of columns in the matrix.
+ */
+    Matrix(int rows, int columns);
 
-    Matrix(std::initializer_list<std::initializer_list<double>> list) {
-        for (auto row : list) {
-            std::vector<double> r;
-            for (auto elem : row) {
-                r.push_back(elem);
-            }
-            matrix.push_back(r);
-        }
-    }
+/**
+ * Constructor for the Matrix class.
+ * 
+ * Creates a Matrix object from a given initializer list.
+ * 
+ * @param list The initializer list to create the Matrix from.
+ */
+    Matrix(std::initializer_list<std::initializer_list<double>> list);
+
 /**
  * @brief Swaps the matrix with another matrix
  * 
  * @param mat_1 The matrix to be swapped with
  */
-    void swap_matrix(Matrix& mat_1) {
-        std::swap(matrix, mat_1.matrix);
-    }
+    void swap_matrix(Matrix& mat_1);
 
 /**
  * pow_matrix()
@@ -53,50 +56,57 @@ public:
  * 
  * @param x - the integer to raise each element of the matrix to
  */
-    void pow_matrix(int x ){
-        for (int i = 0; i < matrix.size(); i++) {
-            for (int j = 0; j < matrix[i].size(); j++) {
-                matrix[i][j] = pow(matrix[i][j], x);
-            }
-        }
-    }
+    void pow_matrix(int x );
     
 /**
  * Calculates the determinant of a matrix.
  * 
  * @return double  The determinant of the matrix
  */
-    double det (){ 
-        int n = matrix.size(); // size of the matrix
-        double det = 1; // determinant
-        // Search for maximum in this column
-        for (int i = 0; i < n; i++) 
-        {
-            int k = i;
-            for (int j = i + 1; j < n; j++)
-                if (abs(matrix[j][i]) > abs(matrix[k][i]))
-                    k = j;
-            if (abs(matrix[k][i]) < 1e-9)
-            {
-                det = 0;
-                break;
-            }
-            // Swap the maximum row with current row (column by column)
-            std::swap(matrix[i], matrix[k]);
-            if (i != k)
-                det = -det;
-            det *= matrix[i][i];
-            // Make all rows below this one 0 in current column 
-            for (int j = i + 1; j < n; j++)
-                matrix[i][j] /= matrix[i][i];
-            for (int j = 0; j < n; j++)
-                if (j != i && abs(matrix[j][i]) > 1e-9)
-                    for (int k = i + 1; k < n; k++)
-                        matrix[j][k] -= matrix[i][k] * matrix[j][i];
-        }
-        return det;
-    }
+    int Determinant ();
 
+/**
+ * T() is a function that transposes a matrix and swaps it with the original matrix.
+ * 
+ * @param matrix The matrix to be transposed and swapped.
+ */
+    void T ();
+
+/**
+ * Calculates the minor of a matrix at a given row and column
+ * 
+ * @param row The row of the matrix to calculate the minor of
+ * @param column The column of the matrix to calculate the minor of
+ * 
+ * @return The minor of the matrix at the given row and column
+ */
+
+    Matrix Minor(int row, int column);
+
+/**
+ * Calculates the inverse of a matrix.
+ * 
+ * @return the inverse of the matrix
+ */
+    Matrix Inverse();
+
+/**
+ * check_Diagonal()
+ * 
+ * Checks if the elements in the diagonal of a matrix are equal.
+ * 
+ * @return bool - Returns true if the elements in the diagonal are equal, false otherwise.
+ */
+    bool check_Diagonal();
+
+/**
+ * Gaussian_8() is a function that implements the Gaussian elimination algorithm to solve a system of linear equations.
+ * 
+ * @param b The vector of constants from the linear equations.
+ * @return A vector of the solutions to the linear equations.
+ */
+    std::vector<double> Gaussian_8(std::vector<double> b);
+    
 /**
 * Straight_Gaussian_stroke()
 * 
@@ -106,7 +116,230 @@ public:
 * @param matrix The matrix to be solved
 * @return x The vector of solutions
 */
-    std::vector<double> Straight_Gaussian_stroke() {
+    std::vector<double> Straight_Gaussian_stroke();
+
+/**
+ * Reverse_Gaussian_stroke() is a function that takes a matrix and returns a vector of doubles.
+ * It uses the Gaussian elimination method to solve a system of linear equations.
+ * 
+ * @param matrix The matrix to be solved
+ * @return A vector of doubles containing the solution to the system of linear equations
+ */
+    std::vector<double> Reverse_Gaussian_stroke();
+
+/**
+ * Adds two matrices together and returns the result
+ * @param m The matrix to add to the current matrix
+ * @return The result of the addition
+ */
+    friend Matrix operator + (Matrix one, Matrix two);
+/**
+ * Subtracts the elements of the given matrix from the elements of the current matrix.
+ * 
+ * @param m The matrix to subtract from the current matrix.
+ * @return The matrix resulting from the subtraction of the two matrices.
+ */
+    friend Matrix operator - (Matrix one, Matrix two);
+
+/**
+ * This function multiplies two matrices and returns the result.
+ * 
+ * @param m The matrix to be multiplied with the current matrix.
+ * @return The result of the multiplication.
+ */
+  friend Matrix operator * (Matrix one, Matrix two);
+
+/**
+ * Multiplies a matrix by a given number.
+ * @param number The number to multiply the matrix by.
+ * @return The resulting matrix after multiplication.
+ */
+    Matrix operator * (double number) {
+        Matrix result;
+        for (int i = 0; i < matrix.size(); i++) {
+            std::vector<double> row;
+            for (int j = 0; j < matrix[i].size(); j++) {
+                row.push_back(matrix[i][j] * number);
+            }
+            result.matrix.push_back(row);
+        }
+        return result;
+    } 
+
+/**
+ * @brief Overloaded operator for outputting a Matrix object to an output stream
+ * 
+ * @param os The output stream to output the Matrix object to
+ * @param m The Matrix object to be outputted
+ * 
+ * @return A reference to the output stream
+ */
+    friend std::ostream& operator<<(std::ostream& os, const Matrix& m);
+
+private:
+    std::vector<std::vector<double>> matrix;
+};
+
+// *********************************************************************************************************************
+   
+   
+    Matrix Matrix::Minor(int row, int column){
+        Matrix result;
+        for (int i = 0; i < matrix.size(); i++) {
+            if (i == row) continue;
+            std::vector<double> row;
+            for (int j = 0; j < matrix[i].size(); j++) {
+                if (j == column) continue;
+                row.push_back(matrix[i][j]);
+            }
+            result.matrix.push_back(row);
+        }
+        return result;
+    }
+    
+    Matrix Matrix::Inverse(){
+        Matrix result;
+        double det = this->Determinant();
+        for (int i = 0; i < matrix.size(); i++) {
+            std::vector<double> row;
+            for (int j = 0; j < matrix[i].size(); j++) {
+                row.push_back(pow(-1, i + j) * Minor(i, j).Determinant());
+            }
+            result.matrix.push_back(row);
+        }
+        result.T();
+        result = result * (1 / det);
+        return result;
+    }
+
+
+    bool  Matrix::check_Diagonal(){
+        for (int i = 0; i < matrix.size(); i++) {
+                if (matrix[i][i] == 0) return false;
+        }
+        return true;
+    } 
+
+    Matrix::Matrix(int rows, int columns, int r) {
+        for (int i = 0; i < rows; i++) {
+            std::vector<double> row;
+            for (int j = 0; j < columns; j++) {
+                row.push_back(rand() % r * (double)1);
+            }
+            matrix.push_back(row);
+        }
+    }
+
+    Matrix::Matrix(int rows, int columns) {
+        for (int i = 0; i < rows; i++) {
+            std::vector<double> row;
+            for (int j = 0; j < columns; j++) {
+                row.push_back(0);
+            }
+            matrix.push_back(row);
+        }
+    }
+
+    Matrix::Matrix(std::initializer_list<std::initializer_list<double>> list) {
+        for (auto row : list) {
+            std::vector<double> r;
+            for (auto elem : row) {
+                r.push_back(elem);
+            }
+            matrix.push_back(r);
+        }
+    }
+
+    void Matrix::swap_matrix(Matrix& mat_1) {
+        std::swap(matrix, mat_1.matrix);
+    }
+
+    void Matrix::pow_matrix(int x ){
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[i].size(); j++) {
+                matrix[i][j] = pow(matrix[i][j], x);
+            }
+        }
+    }
+
+
+    int Matrix::Determinant() {
+        int n = matrix.size();
+        int det = 0;
+        if (n == 1) {
+            return matrix[0][0];
+        }
+        if (n == 2) {
+            return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+        }
+        for (int i = 0; i < n; i++) {
+            Matrix mat = Minor(0, i);
+            det += pow(-1, i) * matrix[0][i] * mat.Determinant();
+        }
+        return det;   
+    }
+
+
+    void Matrix::T () { // Transpose a matrix
+        Matrix mat(matrix[0].size(), matrix.size());
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j <matrix[0].size(); j++) {
+                mat.matrix[j][i] = matrix[i][j];
+            }
+        }
+        swap_matrix(mat);
+    }
+
+
+    std::vector<double> Matrix::Gaussian_8(std::vector<double> b) {
+        int n = matrix.size();
+        // Search for maximum in this column 
+        for (int k = 0; k < n; k++)
+        {
+        // Find the maximum element in the column 
+            int i_max = k;
+            int val_max = abs(matrix[k][k]);
+            for (int i = k + 1; i < n; i++)
+            {
+                if (abs(matrix[i][k]) > val_max)
+                {
+                    i_max = i;
+                    val_max = abs(matrix[i][k]);
+                }
+            }
+
+            // Swap the maximum row with current row (column by column)
+            std::swap(matrix[i_max], matrix[k]);
+            std::swap(b[i_max], b[k]);
+
+            // Make all rows below this one 0 in current column 
+            for (int i = k + 1; i < n; i++)
+            {
+                double c = -matrix[i][k] / matrix[k][k];
+                for (int j = k; j < n; j++)
+                {
+                    if (k == j)
+                        matrix[i][j] = 0;
+                    else
+                        matrix[i][j] += c * matrix[k][j];
+                }
+                b[i] += c * b[k];
+            }
+        }
+
+        // Solve equation Ax=b for an upper triangular matrix A 
+        std::vector<double> x(n);
+        for (int i = n - 1; i >= 0; i--)
+        {
+            x[i] = b[i];
+            for (int j = i + 1; j < n; j++)
+                x[i] -= matrix[i][j] * x[j];
+            x[i] /= matrix[i][i];
+        }
+        return x;
+    }
+
+    std::vector<double> Matrix::Straight_Gaussian_stroke() {
         int n = matrix.size();
         // Search for maximum in this column 
         for (int k = 0; k < n; k++)
@@ -154,14 +387,8 @@ public:
         return x;
     }
 
-/**
- * Reverse_Gaussian_stroke() is a function that takes a matrix and returns a vector of doubles.
- * It uses the Gaussian elimination method to solve a system of linear equations.
- * 
- * @param matrix The matrix to be solved
- * @return A vector of doubles containing the solution to the system of linear equations
- */
-    std::vector<double> Reverse_Gaussian_stroke() {
+
+    std::vector<double> Matrix::Reverse_Gaussian_stroke() {
         int n = matrix.size();
         // Search for maximum in this column 
         std::vector<double> x(n);
@@ -178,90 +405,60 @@ public:
         return x;
     }
 
-/**
- * Adds two matrices together and returns the result
- * @param m The matrix to add to the current matrix
- * @return The result of the addition
- */
-    Matrix operator+(const Matrix& m) {
-        Matrix result;
-        for (int i = 0; i < matrix.size(); i++) {
-            std::vector<double> row;
-            // Add the elements of the given matrix to the elements of the current matrix 
-            for (int j = 0; j < matrix[i].size(); j++) {
-                row.push_back(matrix[i][j] + m.matrix[i][j]);
-            }
-            result.matrix.push_back(row);
-        }
-        return result;
-    } 
-
-   
-/**
- * Subtracts the elements of the given matrix from the elements of the current matrix.
- * 
- * @param m The matrix to subtract from the current matrix.
- * @return The matrix resulting from the subtraction of the two matrices.
- */
-    Matrix operator - (const Matrix& m){
-        Matrix result;
-        for (int i = 0; i < matrix.size(); i++) {
-            std::vector<double> row;
-            // Subtract the elements of the given matrix from the elements of the current matrix
-            for (int j = 0; j < matrix[i].size(); j++) {
-                row.push_back(matrix[i][j] - m.matrix[i][j]);
-            }
-            result.matrix.push_back(row);
-        }
-        return result; 
-    }
-    
-/**
- * This function multiplies two matrices and returns the result.
- * 
- * @param m The matrix to be multiplied with the current matrix.
- * @return The result of the multiplication.
- */
-    Matrix operator * (const Matrix& m) {
-    Matrix result(m.matrix.size(), m.matrix.size());
-    for (int i = 0; i < m.matrix.size(); i++) {
-        for (int k = 0; k < m.matrix.size(); k++) {
-            for (int j = 0; j < m.matrix.size(); j++) 
-                result.matrix[i][j] += matrix[i][k] * m.matrix[k][j];
-        }
-    }
-
-        return result;
-    } 
-/**
- * Multiplies a matrix by a given number.
- * @param number The number to multiply the matrix by.
- * @return The resulting matrix after multiplication.
- */
-    Matrix operator * (int number) {
-        Matrix result;
-        for (int i = 0; i < matrix.size(); i++) {
-            std::vector<double> row;
-            for (int j = 0; j < matrix[i].size(); j++) {
-                row.push_back(matrix[i][j] * number);
-            }
-            result.matrix.push_back(row);
-        }
-        return result;
-    } 
-
-    friend std::ostream& operator<<(std::ostream& os, const Matrix& m);
-
-private:
-    std::vector<std::vector<double>> matrix;
-};
 
 std::ostream& operator<<(std::ostream& os, const Matrix& m) {
     for (int i = 0; i < m.matrix.size(); i++) {
         for (int j = 0; j < m.matrix[i].size(); j++) {
-            os << m.matrix[i][j] << " ";
+            os << m.matrix[i][j] << "\t";
         }
         os << std::endl;
     }
     return os;
 }
+
+Matrix operator * (Matrix one, Matrix two) {
+    Matrix result(one.matrix.size(), one.matrix.size());
+    for (int i = 0; i < one.matrix.size(); i++) {
+        for (int k = 0; k < one.matrix.size(); k++) {
+            for (int j = 0; j < one.matrix.size(); j++) 
+                result.matrix[i][j] += one.matrix[i][k] * two.matrix[k][j];
+        }
+    }
+
+        return result;
+  }
+
+    Matrix operator - (Matrix one, Matrix two) {
+        Matrix result;
+        for (int i = 0; i < one.matrix.size(); i++) {
+            std::vector<double> row;
+            // Subtract the elements of the given matrix from the elements of the current matrix
+            for (int j = 0; j < one.matrix[i].size(); j++) {
+                row.push_back(one.matrix[i][j] - two.matrix[i][j]);
+            }
+            result.matrix.push_back(row);
+        }
+        return result;
+    }
+
+    Matrix operator + (Matrix one, Matrix two) {
+        Matrix result;
+        for (int i = 0; i < one.matrix.size(); i++) {
+            std::vector<double> row;
+            // Add the elements of the given matrix to the elements of the current matrix 
+            for (int j = 0; j < one.matrix[i].size(); j++) {
+                row.push_back(one.matrix[i][j] + two.matrix[i][j]);
+            }
+            result.matrix.push_back(row);
+        }
+        return result;
+    }
+
+
+std::ostream& operator<<(std::ostream& os, const std::vector<double>& vec) {
+    for (int i = 0; i < vec.size(); i++) {
+        os << "x"<< i +1<<": "<< vec[i] << std::endl;
+    }
+    os << std::endl;
+    return os;
+} // *********************************************************************************************************************
--- Computational_Mathematics/LB_1/example.cpp
+++ Computational_Mathematics/LB_1/example.cpp
@@ -2,34 +2,43 @@
 #include "class_Matrix\Matrix.h"
 #include <ctime>  
 
-#define SIZE 3
-
-std::ostream& operator<<(std::ostream& os, const std::vector<double>& vec) {
-    for (int i = 0; i < vec.size(); i++) {
-        os << vec[i] << " ";
-    }
-    os << std::endl;
-    return os;
-} 
-
+/** 
+* @brief This is the main function of the program
+* @return int
+*/
 int main() {
 
-    Matrix matrix_1(SIZE, SIZE, 100);
-    Matrix matrix_2(SIZE,SIZE,100);
-    Matrix result;
+    Matrix exis_1({{1,2,-2,7}, {4,1,2,-2}, {2,2,1,-1}});
+    Matrix exis_1_2 = exis_1;
+    
+    Matrix exis_2({{5,-2,3},{3,-5,3},{8,-4,0}});
+    Matrix exis_2_check = exis_2.Inverse(), res_check,answer;
+    
+    Matrix exis_3_A({{4,0,-6},{1,-1,5},{7,8,-4}}),
+    exis_3_B({{-3,2,1},{7,-7,2},{6,4,5}});
+    
+    std::cout << "====================" << std::endl;
+    std::cout << "Task 1: 0.5 * A + B*A + 2B" << std::endl;
+    answer =  (exis_3_A * 0.5) + exis_3_B * exis_3_A + (exis_3_B * 2) ;
+    std::cout << "Answer:\n" << answer << std::endl; 
 
-    std::cout << "Matrix 1:\n " <<matrix_1 << std::endl; 
+    std::cout << "====================" << std::endl;
+    std::cout << "Task 2" << std::endl;
+    std::cout << "====================" << std::endl;
+    std::cout << "1)Det: " << exis_2.Determinant() << std::endl;
+    std::cout << "2)A^-1:\n" << exis_2_check << std::endl;
+    res_check = exis_2_check * exis_2;
+    std::cout << "Check answer: " << (res_check.check_Diagonal() ? "True" : "False") << std::endl; 
 
-    std::cout << "Matrix 2:\n " << matrix_2 << std::endl; 
-
-    std::cout << "==============" << std::endl;
-
-   result = matrix_1 * matrix_2; 
-
-    std::cout << "Result:\n " << result << std::endl;
-
-    std::cout << "Determinant: " << matrix_1.det() << std::endl; 
+    std::cout << "====================" << std::endl;
+    std::vector<double> x_straight,x_reverse; 
+    std::cout << "Task 3" << std::endl;
+    std::cout << "====================" << std::endl;
+    //x_straight = exis_1.Reverse_Gaussian_stroke(); 
+    x_reverse = exis_1_2.Straight_Gaussian_stroke();
+    std::cout << "x_straight: " << std::endl << x_straight; 
+    std::cout << "x_reverse: " << std::endl << x_reverse; 
+    std::cout << "====================" << std::endl;
 
     return 0;
-
 }
